ec2369f96f55bf5b2e802ac890b9a65107d53f19
farhad <farhadrezaie73@gmail.com>
2022-10-20 06:04:27 +0330

    some changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,13 +67,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    extend(config, { isDev, isClient }) {
-      config.node = {
-        fs: 'empty',
-      }
-
-      // ....
-    },
+    
   },
 
   env: {
